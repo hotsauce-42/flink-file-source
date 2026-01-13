9f6eb968fd663a639ef9e1997f69e0e7f548de08
hotsauce42 <hotsauce42@tutanota.com>
2026-01-13 17:02:44 +0100

    little testing

--- src/main/java/org/example/filesource/Main.java
+++ src/main/java/org/example/filesource/Main.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.time.Duration;
 import java.util.List;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -13,8 +14,8 @@ public class Main {
     // System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "off");
 
     StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-    // env.setParallelism(1);
     env.enableCheckpointing(5000);
+    env.disableOperatorChaining();
 
     Path dirToWatch = Path.fromLocalFile(new File("src/main/resources/testDirectory"));
 
@@ -28,6 +29,6 @@ public class Main {
 
     stream.print();
 
-    env.execute();
+    env.execute("flink source function");
   }
 }
